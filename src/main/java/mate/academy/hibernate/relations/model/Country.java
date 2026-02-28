d90bb1952c4161208effed5f33c2947b7104d660
Szymon Twardowski <szytwardowski@gmail.com>
2026-02-28 18:41:50 +0100

    relations

--- src/main/java/mate/academy/hibernate/relations/model/Country.java
+++ src/main/java/mate/academy/hibernate/relations/model/Country.java
@@ -1,10 +1,12 @@
 package mate.academy.hibernate.relations.model;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
+@Entity
 @Table(name = "countries")
 public class Country implements Cloneable {
     @Id
